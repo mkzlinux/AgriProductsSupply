1b0b12a9953cdc4fe50ab8b7825d233da500a63c
mkzlinux <127420775+mkzlinux@users.noreply.github.com>
2023-05-29 13:31:32 +0200

    Update checkproduct.php

--- checkproduct.php
+++ checkproduct.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Fibble - Supply Chain DAPP</title>
+    <title>FarmBlock - Supply Chain DAPP</title>
     <link rel="SHORTCUT ICON" href="images/fibble.png" type="image/x-icon" />
     <link rel="ICON" href="images/fibble.png" type="image/ico" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
